a2428644005c7ace7d267ad86f19963a69c2b4a5
Panagiotis Skarlas <panossk2@gmail.com>
2023-01-27 19:44:27 +0200

    Improve CI

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements = [
 
 setup(
     name='django-recaptcha-enterprise',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=install_requirements,
     include_package_data=True,
