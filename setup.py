6f8313e08ee94b1165c6a0323d973607d82440de
Panagiotis Skarlas <panossk2@gmail.com>
2023-01-27 21:52:53 +0200

    cleanup gitignore

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requirements = [
 
 setup(
     name='django-recaptcha-enterprise',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=install_requirements,
     include_package_data=True,
